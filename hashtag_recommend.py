d2c9eddf5bfc27f0ba4e3fe7b5c12492a22c92a4
HWhale <masking112@gmail.com>
2019-05-01 16:51:34 +0900

    [ADD] Tag Preprosessor

--- hashtag_recommend.py
+++ hashtag_recommend.py
@@ -14,25 +14,11 @@ import requests
 import os
 import csv
 from matplotlib import pyplot as plt
+from PIL import Image
+import keras
 
-# list of the user dataset
-user_list = ['kotteunji']
-
-def get_hash_data(name, dir_name):
-    kkma = Kkma()
-    filename = os.path.join(os.getcwd(), dir_name, name + '.csv')
-    with open(filename, 'r', encoding='utf-8') as file:
-        reader = csv.reader(file)
-        # column name 제거
-        for line in reader:
-            tags = line[1].split('#')[1:]
-            for tag in tags:
-                words = kkma.pos(tag, flatten=False)
-                #print(tag, words)
-                if len(words) > 1:
-                    print(tag, words)    
-
-get_hash_data('kotteunji', 'data')
+from keras.applications.vgg19 import VGG19
+from keras.applications.vgg19 import preprocess_input
 
 
 def get_insta_image(id):
@@ -41,8 +27,52 @@ def get_insta_image(id):
         os.makedirs('./images')
     with open('./images/' + id + '.jpg' , 'wb') as handler:
         handler.write(img_data)
+
+
+# list of the user dataset
+user_list = ['godjp']
+
+def get_hash_data(name, dir_name):
+    kkma = Kkma()
+    filename = os.path.join(os.getcwd(), dir_name, name + '.csv')
+    with open(filename, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
         
-get_insta_image('Bbq2RxWjkTL')
+        # delete column name
+        next(reader)
+        
+        # for each row, extract post_id and hashtags
+        for line in reader:
+            post_id = line[0].split('/')[4]
+            print(post_id)
+            if not os.path.isfile('./images/'+post_id+'.jpg'):
+                get_insta_image(post_id)
+                print("donwload: " + post_id)
+            #print(post_id)
+            tags = line[1].split('#')[1:]
+            for tag in tags:
+                words = kkma.pos(tag, flatten=False)
+                #print(tag, words)
+                if len(words) > 1:
+                    pass
+                    #print(tag, words)    
+
+for user in user_list:
+    get_hash_data(user, 'data')
+
+
+
+# set image to fixed size
+image = Image.open('./images/BscipNvFcqi.jpg').resize((320, 320))
+image = np.array(image)
+image = preprocess_input(image)
+
+# get image feature from pretrained model
+model = VGG19(weights='imagenet', include_top=False)
+feature = model.predict(image.reshape((1, 320, 320, 3)))
+
+
+
 
 
 model = api.load('glove-twitter-50')
